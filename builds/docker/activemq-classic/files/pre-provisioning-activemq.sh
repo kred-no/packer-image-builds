d1aa44f4cb1f2e146f6a4b02acbff44cfd64c799
Rune Rønneseth <rune.ronneseth@kred.no>
2024-04-22 16:48:20 +0200

    permissions

--- builds/docker/activemq-classic/files/pre-provisioning-activemq.sh
+++ builds/docker/activemq-classic/files/pre-provisioning-activemq.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -eu
+set -exu
 
 log(){
   printf "[%s] %s\n" "$(date -u)" "$1"
@@ -9,7 +9,7 @@ log(){
 if [ -z "activemq_version" ]; then log "Please set the 'activemq_version' variable";exit 1; fi
 
 #tempdir=./cache
-tempdir=./files/cache
+tempdir="./files/cache"
 mkdir -p ${tempdir}
 pushd ${tempdir}
 
@@ -20,7 +20,7 @@ pushd ${tempdir}
 activemq_url="https://archive.apache.org/dist/activemq/${activemq_version}/apache-activemq-${activemq_version}-bin.tar.gz"
 activemq_filename="apache-activemq"
 
-log "[ActiveMQ] Processing v${activemq_version}"
+log "[ActiveMQ] Processing v${activemq_version} -> ${tempdir}"
 curl --silent --retry 3  -L "${activemq_url}.sha512" -o "${activemq_filename}.sha512"
 
 if [[ -f "${activemq_filename}-bin.tar.gz" ]]; then
--- builds/docker/activemq-classic/files/pre-provisioning-postgres.sh
+++ builds/docker/activemq-classic/files/pre-provisioning-postgres.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -eu
+set -exu
 
 log(){
   printf "[%s] %s\n" "$(date -u)" "$1"
